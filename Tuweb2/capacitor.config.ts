a1914f8c8006ed6bb5a9f07bb2a8f258be154687
afandi-nat <afandi@polines.ac.id>
2023-12-03 16:05:18 +0700

    add tuweb2

---
+++ Tuweb2/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tuweb2',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
---
+++ Tuweb2/src/views/HomePage.vue
@@ -0,0 +1,51 @@
+<template>
+  <ion-content>
+    <ion-grid>
+      <ion-row>
+        <ion-col size="12" class="ion-text-center" style="text-align: center;">
+          <ion-button @click="getData" style="text-transform: capitalize;">
+            Get Data
+          </ion-button>
+        </ion-col>
+      </ion-row>
+
+      <ion-row v-if="items.length > 0" style="border: 2px solid black; padding:
+      20px;">
+        <ion-col size="4" style="text-align: center;">Name</ion-col>
+        <ion-col size="4" style="text-align: center;">Symbol</ion-col>
+        <ion-col size="4" style="text-align: center;">Harga USD</ion-col>
+      </ion-row>
+
+      <ion-row v-for="item in items" :key="id">
+        <ion-col size="4" style="text-align: center; padding: 10px;">{{ item.name
+        }}</ion-col>
+        <ion-col size="4" style="text-align: center; padding: 10px;">{{
+          item.symbol }}</ion-col>
+        <ion-col size="4" style="text-align: center; padding: 10px;">{{
+          item.price_usd }}</ion-col>
+      </ion-row>
+    </ion-grid>
+  </ion-content>
+</template>
+  
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  methods: {
+    getData() {
+      axios.get('https://api.coinlore.net/api/tickers/')
+        .then(response => {
+          this.items = response.data.data
+        }).catch(error => {
+          console.log(error);
+        });
+    }
+  },
+}
+</script>
+  
